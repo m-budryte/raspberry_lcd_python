eb9ecabea39ca55e25c22470939191adf761802b
Masha <mashabudryte@gmail.com>
2021-01-10 19:29:48 +0000

    coding defined

--- hello_world_lcd.py
+++ hello_world_lcd.py
@@ -6,32 +6,35 @@ from time import *
 
 mylcd = RPi_I2C_driver.lcd()
 
-input_value = ""
+fontdata1 = [      
+        [ 0b01110, 
+          0b01110, 
+          0b00100, 
+          0b00100, 
+          0b00100, 
+          0b00100, 
+          0b11111, 
+          0b11011 ],
+        [ 0b01110, 
+          0b01110, 
+          0b00100, 
+          0b00100, 
+          0b00100, 
+          0b00100, 
+          0b11111, 
+          0b11011 ],
+        [ 0b01110, 
+          0b01110, 
+          0b00100, 
+          0b00100, 
+          0b00100, 
+          0b00100, 
+          0b11111, 
+          0b11011 ],]
 
-while input_value != "exit":
-    input_value = raw_input("Enter your msg: ") 
-
-    fontdata1 = [      
-            [ 0b00010, 
-            0b00100, 
-            0b01000, 
-            0b10000, 
-            0b01000, 
-            0b00100, 
-            0b00010, 
-            0b00000 ],
-    ]
-
-    mylcd.lcd_load_custom_chars(fontdata1)
-    mylcd.lcd_write(0x80)
-    mylcd.lcd_write_char(0)
-
-    if input_value == "balance":
-        mylcd.lcd_display_string("Your balance is ",  1)
-        
-
-    sleep(5)
-
-    mylcd.lcd_clear()
-
+mylcd.lcd_load_custom_chars(fontdata1)
+mylcd.lcd_write(0x80)
+mylcd.lcd_write_char(0)
+mylcd.lcd_write_char(1)
+mylcd.lcd_write_char(2)
 
