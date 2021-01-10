6012fa60d64512d24e36d46c68a502042f26eb02
Masha <mashabudryte@gmail.com>
2021-01-10 15:57:48 +0000

    strobe

--- hello_world_lcd.py
+++ hello_world_lcd.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import I2C_LCD_driver
-import time
+from time import *
+
 mylcd = I2C_LCD_driver.lcd()
-mylcd.lcd_strobe()
+
+mylcd.lcd_display_string("This is how you", 1)
+sleep(1)
+
+mylcd.lcd_clear()
+
+mylcd.lcd_display_string("clear the screen", 1)
+sleep(1)
+
+mylcd.lcd_clear()
