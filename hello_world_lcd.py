4f7c693a81c1289c13c0ec2e402e8b9ab43bd04f
Masha <mashabudryte@gmail.com>
2021-01-10 17:31:32 +0000

    console to screen

--- hello_world_lcd.py
+++ hello_world_lcd.py
@@ -5,10 +5,14 @@ from time import *
 
 mylcd = RPi_I2C_driver.lcd()
 
-while True:
+input_value = ""
+
+while input_value != "exit":
     input_value = raw_input("Enter your value: ") 
 
     mylcd.lcd_display_string(input_value, 1)
-    sleep(1)
+    
+    sleep(5)
 
-    mylcd.lcd_clear()
+mylcd.lcd_clear()
+
