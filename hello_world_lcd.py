457dfd6fbd493bd3e22301a19a8d0b1890543664
Masha <mashabudryte@gmail.com>
2021-01-10 21:24:36 +0000

    coding defined

--- hello_world_lcd.py
+++ hello_world_lcd.py
@@ -6,39 +6,45 @@ from time import *
 
 mylcd = RPi_I2C_driver.lcd()
 
-fontdata1 = [      
-        [ 0b01110, 
-          0b01110, 
-          0b00100, 
-          0b00100, 
-          0b00100, 
-          0b00100, 
+full_square = [ 0b11111, 
           0b11111, 
-          0b11011 ],
-        [ 0b01110, 
-          0b01110, 
-          0b00100, 
-          0b00100, 
-          0b00100, 
-          0b00100, 
           0b11111, 
-          0b11011 ],
-        [ 0b01110, 
-          0b01110, 
-          0b00100, 
-          0b00100, 
-          0b00100, 
-          0b00100, 
           0b11111, 
-          0b11011 ],]
+          0b11111, 
+          0b11111, 
+          0b11111, 
+          0b11111 ]
 
-mylcd.lcd_load_custom_chars(fontdata1)
+empty_square = [ 0b00000, 
+          0b00000, 
+          0b00000,
+          0b00000,
+          0b00000, 
+          0b00000, 
+          0b00000,
+          0b00000 ]
+
+top_row = [full_square, full_square, full_square, full_square, full_square, full_square]
+middle_row = [full_square, full_square, full_square, full_square, full_square, full_square]
+bottom_row = [full_square, full_square, full_square, full_square, full_square, full_square]
+
+mylcd.lcd_load_custom_chars(top_row)
 
 mylcd.lcd_write(0x80)
 mylcd.lcd_write_char(0)
 mylcd.lcd_write_char(1)
 mylcd.lcd_write_char(2)
+
+mylcd.lcd_load_custom_chars(middle_row)
+
+
+mylcd.lcd_write(0xC0)
+mylcd.lcd_write_char(0)
+mylcd.lcd_write_char(1)
+mylcd.lcd_write_char(2)
+
 sleep(5)
+
 mylcd.lcd_clear()
 
 
