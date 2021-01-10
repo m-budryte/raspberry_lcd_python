13975a9afcaf08b6c75c97772ec3bd56426c3a0f
Masha <mashabudryte@gmail.com>
2021-01-10 17:23:10 +0000

    console to screen

--- hello_world_lcd.py
+++ hello_world_lcd.py
@@ -8,7 +8,7 @@ mylcd = RPi_I2C_driver.lcd()
 val = input("Enter your value: ") 
 print(val) 
 
-mylcd.lcd_display_string(val, 1)
+mylcd.lcd_display_string(str(val), 1)
 sleep(1)
 
 mylcd.lcd_clear()
