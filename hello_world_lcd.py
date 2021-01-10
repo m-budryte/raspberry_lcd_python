62efe122630bc226eb6166c3c980aec9990894f3
Masha <mashabudryte@gmail.com>
2021-01-10 18:56:12 +0000

    coding defined

--- hello_world_lcd.py
+++ hello_world_lcd.py
@@ -11,8 +11,8 @@ while input_value != "exit":
     input_value = raw_input("Enter your msg: ") 
 
     if input_value == "balance":
-        mylcd.lcd_display_string("Your Revolut balance U+00A30.00", 1)
-        mylcd.lcd_display_string("Your Barc balance is U+00A30.00", 2)
+        mylcd.lcd_display_string("Your Revolut balance &#xA3;0.00", 1)
+        mylcd.lcd_display_string("Your Barc balance is &#xA3;0.00", 2)
 
     sleep(5)
 
