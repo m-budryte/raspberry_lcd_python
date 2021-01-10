7fa475b7d221daada2c0c96d21a6d7940e73ceda
Masha <mashabudryte@gmail.com>
2021-01-10 15:32:37 +0000

    update address

--- I2C_LCD_driver.py
+++ I2C_LCD_driver.py
@@ -106,7 +106,7 @@ Rs = 0b00000001 # Register select bit
 class lcd:
    #initializes objects and lcd
    def __init__(self):
-      self.lcd_device = i2c_device(ADDRESS)
+      self.lcd_device = i2c_device(0x27)
 
       self.lcd_write(0x03)
       self.lcd_write(0x03)
